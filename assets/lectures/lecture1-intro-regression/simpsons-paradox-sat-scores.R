89775ddff08354067b0ba6c2df0eb9027e845922
Nicholas Reich <nick@schoolph.umass.edu>
2016-01-15 21:44:28 -0800

    updates to lecture 1, adding instructor schedule.

--- assets/lectures/lecture1-intro-regression/simpsons-paradox-sat-scores.R
+++ assets/lectures/lecture1-intro-regression/simpsons-paradox-sat-scores.R
@@ -1,4 +1,5 @@
 library(mosaic)
+library(mosaicData)
 
 qplot(salary, sat, data=SAT)
 
@@ -23,3 +24,12 @@ qplot(expend, sat, data=SAT,  geom=c("point", "smooth"), method="lm", se=FALSE)
 
 qplot(ratio, sat, data=SAT)
 qplot(ratio, sat, data=SAT,  geom=c("point", "smooth"), method="lm", se=FALSE)
+
+ggplot(SAT, aes(x=salary, y=sat)) +
+    geom_smooth(aes(color=fracgrp), method="lm", se=FALSE) +
+    geom_point(aes(color=fracgrp)) +
+    geom_smooth(method="lm", se=FALSE, color="gray", linetype=2) + 
+    xlab("estimated average public school teacher salary") + 
+    ylab("average total SAT score, by state")+
+    scale_color_discrete("% students\ntaking SAT")
+
